9b2144e9639a1770b1cf54313c344316418396b5
Jun-Min Lee <ljm56897@gmail.com>
2026-02-06 15:56:43 +0900

    feat: Incorporate data generation codes (generation, fhir conversion, and building agent data)

--- src/run/synthesize_data.py
+++ src/run/synthesize_data.py
@@ -6,7 +6,7 @@ from sconf import Config
 from argparse import ArgumentParser
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from h_adminsim.tools import DataSynthesizer
+from h_adminsim.pipeline import DataGenerator
 from h_adminsim.utils import log
 
 
@@ -29,20 +29,21 @@ def main(args):
     # Init environment
     env_setup(config)
 
-    # Initialize data synthesizer
-    data_synthesizer = DataSynthesizer(config)
-    try:
-        data, hospital_obj = data_synthesizer.synthesize(sanity_check=args.sanity_check)
-        log(f"Data synthesis completed successfully", color=True)
-    except Exception as e:
-        log("Data synthesis failed.", level='error')
-        raise e
+    # Generate data
+    data_generator = DataGenerator(config=config)
+    output = data_generator.build(
+        sanity_check=args.sanity_check,
+        convert_to_fhir=args.convert_to_fhir,
+        build_agent_data=True
+    )
+
     
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-c', '--config', type=str, required=True, help='Path to the configuration file')
     parser.add_argument('--sanity_check', action='store_true', required=False, help='Check whether generated data and Hospital object compatible')
+    parser.add_argument('--convert_to_fhir', action='store_true', required=False, help='Whether convert generated data to FHIR or not')
     args = parser.parse_args()
 
     main(args)
